ba799cd00d95896b9d4c774242c31d5c19a2d974
Foseos <lucasdachez@yahoo.fr>
2024-03-02 14:50:50 +0100

    Update NavLink.jsx

--- src/app/components/NavLink.jsx
+++ src/app/components/NavLink.jsx
@@ -6,9 +6,10 @@ const NavLink = ({ href, title }) => {
       href={href}
       className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
     >
-      {title = "PortFolio Dachez"}
+      {title}
     </Link>
   );
 };
+
 
 export default NavLink;
